5f4fb485f759714bed4c5abd948cf588931e34f0
janghawon <98999600+janghawon@users.noreply.github.com>
2022-07-13 09:12:54 +0900

    rullet4

--- Gamejam_11/Assets/02_scriptes/random/Random11.cs
+++ Gamejam_11/Assets/02_scriptes/random/Random11.cs
@@ -25,9 +25,25 @@ public class Random11 : MonoBehaviour
     public Sprite Image8;
     public Sprite Image9;
     public Sprite Image10;
-    
+
+    public GameObject Gi1;
+    public GameObject Gi2;
+    public GameObject Gi3;
+    public GameObject Gi4;
+    public GameObject Gi5;
+    public GameObject Gi6;
+    public GameObject Gi7;
+    public GameObject Gi8;
+    public GameObject Gi9;
+    public GameObject Gi10;
+
+
 
     public Sprite Nomoney;
+
+    public GameObject Result_PopUp;
+    public GameObject Result_Btn;
+    
     
 
     // 변수
@@ -50,56 +66,126 @@ public class Random11 : MonoBehaviour
 
             DrawShop.SetActive(false); //뽑기 선택 화면을 비활성화하고,
             DrawWindow.SetActive(true);// 랜덤 이미지를 출력한 화면을 활성화합니다.
-            if (RandomInt == 1) // RandomInt가 1이라면
-            {
-                DrawImage.sprite = Image1; // DrawImage의 Sprite에 Image1(Sprite)를 적용합니다.
-            }
-            else if (RandomInt == 2)
-            {
-                DrawImage.sprite = Image2;
-            }
-            else if (RandomInt == 3)
-            {
-                DrawImage.sprite = Image3;
-            }
-            else if (RandomInt == 4)
-            {
-                DrawImage.sprite = Image4;
-            }
-            else if (RandomInt == 5)
-            {
-                DrawImage.sprite = Image5;
-            }
-            else if (RandomInt == 6)
-            {
-                DrawImage.sprite = Image6;
-            }
-            else if (RandomInt == 7)
-            {
-                DrawImage.sprite = Image7;
-            }
-            else if (RandomInt == 8)
-            {
-                DrawImage.sprite = Image8;
-            }
-            else if (RandomInt == 9)
-            {
-                DrawImage.sprite = Image9;
-            }
-            else if (RandomInt == 10)
-            {
-                DrawImage.sprite = Image10;
-            }
-            Invoke("CloseDraw", 2.0f); // 2초 뒤에 CloseDraw 함수를 실행합니다.
+
+            StartCoroutine("RandomOn");
         }
-        else
-        {
-            Debug.Log("돈이 없습니다.");
-        }
+            
         
 
         
     }
+    IEnumerator RandomOn()
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            DrawImage.sprite = Image1;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image2;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image3;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image4;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image5;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image6;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image7;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image8;
+            yield return new WaitForSeconds(0.04f);
+            DrawImage.sprite = Image9;
+            yield return new WaitForSeconds(0.04f);
+        }
+
+        if (RandomInt == 1) // RandomInt가 1이라면
+        {
+            DrawImage.sprite = Image1;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi1.SetActive(true);
+
+        }
+        else if (RandomInt == 2)
+        {
+            DrawImage.sprite = Image2;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi2.SetActive(true);
+
+        }
+        else if (RandomInt == 3)
+        {
+            DrawImage.sprite = Image3;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi3.SetActive(true);
+
+        }
+        else if (RandomInt == 4)
+        {
+            DrawImage.sprite = Image4;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi4.SetActive(true);
+
+        }
+        else if (RandomInt == 5)
+        {
+            DrawImage.sprite = Image5;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi5.SetActive(true);
+
+        }
+        else if (RandomInt == 6)
+        {
+            DrawImage.sprite = Image6;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi6.SetActive(true);
+
+        }
+        else if (RandomInt == 7)
+        {
+            DrawImage.sprite = Image7;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi7.SetActive(true);
+
+        }
+        else if (RandomInt == 8)
+        {
+            DrawImage.sprite = Image8;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi8.SetActive(true);
+
+        }
+        else if (RandomInt == 9)
+        {
+            DrawImage.sprite = Image9;
+            Result_PopUp.SetActive(true);
+            Result_Btn.SetActive(true);
+
+            Gi9.SetActive(true);
+
+        }
+        Invoke("CloseDraw", 2f);
+
+
+       
+
+    }
+
 
     public void CloseDraw() // 뽑기 스크립트가 실행되고 자동으로 실행되게 합니다.
     {
